7a46244dd9cf90e3a104f3bd196b14e5c8040284
kyw <[duddnr654@naver.com]>
2020-10-19 20:10:57 +0900

    music play button

--- js/button.js
+++ js/button.js
@@ -1,8 +1,15 @@
-playBtn = document.querySelector(".music__play-btn");
 
-playBtn.addEventListener("click", () => {
-    playBtn.classList.add("fa-pause");
-    playBtn.classList.remove("fa-play");
-});
+const playIcon = document.getElementById("music__play-btn");
+const pauseIcon = document.getElementById("music__pause-btn");
+
+playIcon.addEventListener("click", () => {
+    playIcon.style.display = "none"
+    pauseIcon.style.display = "flex"
+})
+
+pauseIcon.addEventListener("click", () => {
+    pauseIcon.style.display = "none"
+    playIcon.style.display = "flex"
+})
 
 // toggle 이용할 수 있도록 해보기.
