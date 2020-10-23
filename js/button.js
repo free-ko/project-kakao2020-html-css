4cc9412eb385afb364d82be657466be48d15e245
free-ko <duddnr654@naver.com>
2020-10-23 16:02:01 +0900

    Try Img Change with JS

--- js/button.js
+++ js/button.js
@@ -1,8 +1,12 @@
 
 const playIcon = document.getElementById("music__play-btn");
+const nextIcon = document.getElementById("music__next-btn");
 const pauseIcon = document.getElementById("music__pause-btn");
-const musicPlayImg = document.querySelector(".music-play__img");
+const musicPlayImg = document.querySelector(".rotate__img");
 const musicPlaying = document.getElementById("music");
+
+const img1 = document.querySelector("music-play__img-1").style.display='block';
+const img2 = document.querySelector("music-play__img-2");
 
 
 playIcon.addEventListener("click", () => {
@@ -17,6 +21,11 @@ pauseIcon.addEventListener("click", () => {
     musicPlaying.style.display = "none"
     pauseIcon.style.display = "none"
     playIcon.style.display = "flex"
+})
+
+nextIcon.addEventListener("click", () => {
+    img1.style.display = "none"
+    img2.style.display = "flex"
 })
 
 // toggle 이용할 수 있도록 해보기.
